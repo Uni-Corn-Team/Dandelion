2e6e6876eff09a5badc92669249b78a92ad8d365
m_a_ks_ <maksim.m00@mail.ru>
2021-05-20 21:24:15 +0300

    add observer

---
+++ DandelionLib/DandelionLib/IObservable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DandelionLib
+{
+    interface IObservable
+    {
+        List<IObserver> Observers { get; }
+
+        void AddObserver(IObserver observer);
+        void DeleteObserver(IObserver observer);
+        void NotifyObservers(object o);
+    }
+}
---
+++ DandelionLib/DandelionLib/UserBar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DandelionLib
+{
+    class UserBar : IObserver
+    {
+        private int _healthBar;
+        private int _hightBar;
+        private int _maxHealth;
+
+        public void Update(object o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int HightBar => _hightBar;
+
+        public double HealthBar => _healthBar / (double)_maxHealth;
+    }
+}
