8a455ef2cbeda27f7f89717932b3e5ce8a5f4d99
m_a_ks_ <maksim.m00@mail.ru>
2021-05-22 11:12:58 +0300

    add adapter IEntity to IUnityEntity

---
+++ DandelionLib/DandelionLib/Enums/EntityType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DandelionLib.Enums
+{
+    enum EntityType
+    {
+        NonType,
+        FallingLittleBomb,
+        FallingMiddleBomb,
+        FallingLargeBomb,
+        FallingUltimateBomb,
+        FallingLittleHealth,
+        FallingMiddleHealth,
+        FallingLargeHealth,
+        FallingUltimateHealth
+    }
+}
